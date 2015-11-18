686b9518f5f47119aa3fe30e75c1c09467b8b57c
RakeshKumarSatvik <satvik@usc.edu>
2015-11-18 03:17:53 -0800

    Phase 3, initial steps.

--- client.c
+++ client.c
@@ -14,6 +14,7 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <limits.h>
 
 #define PORT "3490"  // the port users will be connecting to
 
@@ -47,6 +48,78 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+// Number of vertices in the graph
+#define V 4
+
+typedef int bool;
+enum { false, true };
+
+// A utility function to find the vertex with minimum key value, from
+// the set of vertices not yet included in MST
+int minKey(int key[], bool mstSet[])
+{
+	int v;
+   // Initialize min value
+   int min = INT_MAX, min_index;
+
+   for (v = 0; v < V; v++)
+     if (mstSet[v] == false && key[v] < min)
+         min = key[v], min_index = v;
+
+   return min_index;
+}
+
+// A utility function to print the constructed MST stored in parent[]
+int printMST(int parent[], int n, int graph[V][V])
+{
+	int i;
+   printf("Edge   Weight\n");
+   for (i = 1; i < V; i++)
+      printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
+}
+
+// Function to construct and print MST for a graph represented using adjacency
+// matrix representation
+void primMST(int graph[V][V])
+{
+     int parent[V]; // Array to store constructed MST
+     int key[V];   // Key values used to pick minimum weight edge in cut
+     bool mstSet[V];  // To represent set of vertices not yet included in MST
+     int i, count;
+     // Initialize all keys as INFINITE
+     for (i = 0; i < V; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+
+     // Always include first 1st vertex in MST.
+     key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
+     parent[0] = -1; // First node is always root of MST
+
+     // The MST will have V vertices
+     for (count = 0; count < V-1; count++)
+     {
+    	 int v;
+        // Pick thd minimum key vertex from the set of vertices
+        // not yet included in MST
+        int u = minKey(key, mstSet);
+
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+
+        // Update key value and parent index of the adjacent vertices of
+        // the picked vertex. Consider only those vertices which are not yet
+        // included in MST
+        for (v = 0; v < V; v++)
+
+           // graph[u][v] is non zero only for adjacent vertices of m
+           // mstSet[v] is false for vertices not yet included in MST
+           // Update the key only if graph[u][v] is smaller than key[v]
+          if (graph[u][v] && mstSet[v] == false && graph[u][v] <  key[v])
+             parent[v]  = u, key[v] = graph[u][v];
+     }
+
+     // print the constructed MST
+     printMST(parent, V, graph);
+}
 int main(void)
 {
 	int temp[4], topology[4][4], adjacency[4][4];
@@ -331,6 +404,11 @@ int main(void)
 			printf("talker: sent %d bytes to %s\n", numbytes, "localhost");
 			close(sockfd);
 		}/*End of UDP for portD*/
+	}
+
+	/*Phase 3, calculate the tree*/
+	{
+		primMST(topology);
 	}
 	return 0;
 }
